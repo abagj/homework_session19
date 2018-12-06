890a25e3b29192cf4ef4a4a63be59741bf6c0467
Anikkenbg <anikkenbg@student.ie.edu>
2018-12-06 07:40:16 +0100

    homework_session19

---
+++ homework 06.12.2018.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 07:22:33 2018
+
+@author: anikkengjeruldsen
+"""
+
+#%%
+    
+#Create a function that uses the requests library to get the lyrics of a song.
+#You can use the lyrics.ovh api as described here: 
+#https://lyricsovh.docs.apiary.io/#reference/0/lyrics-of-a-song/search?console=1
+
+import requests
+
+lyrics = requests.get("https://api.lyrics.ovh/v1/Tony Bennett/Winter Wonderland").json()
+
+print(lyrics)
+
+#%%
+
+#Create a function that returns the current latitude and longitude of the ISS
+#http://api.open-notify.org/
+
+import requests
+
+ISS = requests.get("http://api.open-notify.org/iss-now.json").json()
+
+print(ISS["iss_position"]["longitude"])
+print(ISS["iss_position"]["latitude"])
+
+#%%
+
+#using the given population API, create a program that:
+#
+#- gets a list of all available countries
+#
+#- Gets the first 10 countries
+#
+#- Gets the country's today & tomorrow population.
+#
+#http://api.population.io/#!/countries/listCountries
+
+import requests
+
+all_countries = requests.get("http://api.population.io:80/1.0/countries").json()
+
+print(all_countries)
+
